c64cba0d159cd3968e4e5fbf7e51120d7b695822
Ali Can BATUR <baturalican@hotmail.com>
2022-08-08 18:48:57 +0300

    feat: add dynamic server addition

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { timestamp } from 'rxjs';
 
 @Component({
   selector: 'app-servers',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   isDisabled: boolean = true;
-  serverCreationStatus = "No server is created!";
+  serverCreationStatus = "";
   serverName: string = "";
   serverCreated: boolean = false;
+  servers: string[] = [];
+  logs: string[] = [];
+  clickCount: number = 0;
+
+  isTextVisible: boolean = false;
+  secretText: string = "My secret text hehe :)";
 
   constructor() {
     setTimeout(() => {
@@ -21,11 +28,26 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.servers.push(this.serverName);
     this.serverCreationStatus = "Server was created! and Name is: " + this.serverName;
     this.serverCreated = true;
   }
 
   onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  showParagraph() {
+    this.isTextVisible = !this.isTextVisible;
+    let dateTime = new Date();
+    this.logs.push(dateTime.toLocaleDateString() + " " + dateTime.toLocaleTimeString());
+    ++this.clickCount;
+  }
+
+  getNewColor() {
+    if (this.clickCount === 5) {
+      return "Blue";
+    }
+    return "White";
   }
 }
